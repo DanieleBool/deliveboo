0e3f26002b322d9c47b8e9ec075eddc7ad98ce56
AlexsandroD <adrdra88@gmail.com>
2022-03-10 18:11:11 +0100

    Models relationship

--- backend/app/Restaurant.php
+++ backend/app/Restaurant.php
@@ -9,4 +9,13 @@ class Restaurant extends Model
     public function user(){
         return $this->hasOne('App\User');
     }
+    public function categories(){
+        return $this->belongsToMany('App\Category');
+    }
+    public function orders(){
+        return $this->hasMany('App\Order');
+    }
+    public function dishes(){
+        return $this->hasMany('App\Dish');
+    }
 }
