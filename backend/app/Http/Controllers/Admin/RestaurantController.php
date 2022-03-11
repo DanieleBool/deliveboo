9879d17c981f3995e1d8fe0a3f2815d24750c211
AlexsandroD <adrdra88@gmail.com>
2022-03-11 19:11:04 +0100

    restaurant crud delete

--- backend/app/Http/Controllers/Admin/RestaurantController.php
+++ backend/app/Http/Controllers/Admin/RestaurantController.php
@@ -12,18 +12,6 @@ class RestaurantController extends Controller
 {
 
 
-     protected $validationRule = [
-        "name" => "required|string|max:110",
-        "description" => "nullable|string",
-        "email" =>'required|string|max:100|unique:restaurants,email',
-        "address" => "required|string|max:255|unique:restaurants,address",
-        "city" => "required|string|max:100",
-        "country" => "required|string|max:255",
-        "post_code" =>"required|string|max:5",
-        "phone" =>"required|string|max:15|unique:restaurants,phone",
-        "image_cover" =>  "nullable|mimes:jpg,jpeg,png|max:2048",
-    ];
-
     protected $unwanted_array = array('Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
         'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
         'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
@@ -58,7 +46,19 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate($this->validationRule);
+
+        $request->validate([
+            "name" => "required|string|max:110",
+            "description" => "nullable|string",
+            "email" =>'required|string|max:100|unique:restaurants,email',
+            "address" => "required|string|max:255|unique:restaurants,address",
+            "city" => "required|string|max:100",
+            "country" => "required|string|max:255",
+            "post_code" =>"required|string|max:5",
+            "phone" =>"required|string|max:15|unique:restaurants,phone",
+            "image_cover" =>  "nullable|mimes:jpg,jpeg,png|max:2048",
+        ]);
+
         $data = $request->all();
         $newRestaurant = new Restaurant();
         $newRestaurant->fill($data);
@@ -79,7 +79,7 @@ class RestaurantController extends Controller
      */
     public function show($id)
     {
-        //
+        // La pagina index in questo applicativo funge da SHOW
     }
 
     /**
@@ -88,9 +88,9 @@ class RestaurantController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Restaurant $restaurant)
     {
-        //
+        return view('admin.restaurants.edit', compact('restaurant'));
     }
 
     /**
@@ -100,9 +100,39 @@ class RestaurantController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Restaurant $restaurant)
     {
-        //
+        $request->validate([
+        "name" => "required|string|max:110",
+        "description" => "nullable|string",
+        "email" =>'required|string|max:100|unique:restaurants,email,'.$restaurant->id,
+        "address" => "required|string|max:255|unique:restaurants,address,".$restaurant->id,
+        "city" => "required|string|max:100",
+        "country" => "required|string|max:255",
+        "post_code" =>"required|string|max:5",
+        "phone" =>"required|string|max:15|unique:restaurants,phone,".$restaurant->id,
+        "image_cover" =>  "nullable|mimes:jpg,jpeg,png|max:2048",
+        ]);
+
+        $data = $request->all();
+
+
+
+        if($data['name'] != $restaurant->name || $data['address'] != $restaurant->address ){
+            $restaurant->slug = Str::of(strtr( $data['name'].' '.$data['address'], $this->unwanted_array ))->slug('-');
+        }
+
+        // if( isset($data['image']) ) {
+        //     Storage::delete($post->image);
+        //     $path_image = Storage::put("uploads",$data['image']);
+        //     $post->image = $path_image;
+        // }
+
+        $restaurant->fill($data);
+
+        $restaurant->save();
+
+        return redirect()->route('restaurants.index');
     }
 
     /**
@@ -111,8 +141,14 @@ class RestaurantController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Restaurant $restaurant)
     {
-        //
+        //  if($post->image){
+        //     Storage::delete($post->image);
+        // }
+
+        $restaurant->delete();
+
+        return redirect()->route('restaurants.index');
     }
 }
