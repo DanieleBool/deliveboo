5855c93f130e93585d3421eb703d9ca53dc5b042
AlexsandroD <adrdra88@gmail.com>
2022-03-14 10:04:30 +0100

    npm install + watch

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -37335,8 +37335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/leo/Documents/PHP-Root/Laravel/deliveboo/backend/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/leo/Documents/PHP-Root/Laravel/deliveboo/backend/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\ALEX\Desktop\progetto finale\deliveboo\backend\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\ALEX\Desktop\progetto finale\deliveboo\backend\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
